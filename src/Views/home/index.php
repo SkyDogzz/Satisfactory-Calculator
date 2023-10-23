0146bb79c307fe5d9682d3fcb3fbe17769d8adf0
SkyDogzz <thomas.stephan@live.fr>
2023-10-23 03:49:19 +0000

    fix needed calculation

--- src/Views/home/index.php
+++ src/Views/home/index.php
@@ -8,10 +8,15 @@
             const item = data.item.find(i => i.name === itemName);
             if (item && item.recipe && item.recipe.ingredients.length > 0) {
                 const ul = document.createElement('ul');
+                const outputQuantityPerRecipe = item.recipe.output.quantity;
+                // Calculating the number of recipes required
+                const recipesRequired = Math.ceil(quantityNeeded / outputQuantityPerRecipe);
                 for (const ingredient of item.recipe.ingredients) {
                     const li = document.createElement('li');
-                    li.textContent = `${ingredient.item} (Crafted: ${ingredient.quantity * quantityNeeded})`;
-                    li.appendChild(createList(ingredient.item, ingredient.quantity * quantityNeeded));  // Recursively create lists
+                    // Adjusting the ingredient quantity based on the number of recipes required
+                    const ingredientQuantity = ingredient.quantity * recipesRequired;
+                    li.textContent = `${ingredientQuantity} ${ingredient.item}`;
+                    li.appendChild(createList(ingredient.item, ingredientQuantity));  // Recursively create lists
                     ul.appendChild(li);
                 }
                 return ul;
@@ -22,7 +27,7 @@
         const contentDiv = document.getElementById('content');
         const topLevelList = document.createElement('ul');
         const topLevelItem = document.createElement('li');
-        topLevelItem.textContent = `${targetItem} (Crafted: 1)`;
+        topLevelItem.textContent = `1 ${targetItem}`;
         topLevelItem.appendChild(createList(targetItem, 1));
         topLevelList.appendChild(topLevelItem);
         contentDiv.appendChild(topLevelList);
