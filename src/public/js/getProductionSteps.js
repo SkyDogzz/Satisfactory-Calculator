b234501a3ccb70d90730e67b9503de61952d73dd
SkyDogzz <thomas.stephan@live.fr>
2023-10-24 00:47:03 +0000

    add try catch

--- src/public/js/getProductionSteps.js
+++ src/public/js/getProductionSteps.js
@@ -1,33 +1,39 @@
 import fetchItems from './fetchItems.js';
 
 export default async function getProductionSteps(targetItem, quantity) {
-    const data = await fetchItems();
+    try {
+        const data = await fetchItems();
 
-    function createList(itemName, quantityNeeded) {
-        const item = data.item.find(i => i.name === itemName);
-        if (item && item.recipe && item.recipe.ingredients.length > 0) {
-            const ul = document.createElement('ul');
-            const outputQuantityPerRecipe = item.recipe.output.quantity;
-            // Calculating the number of recipes required
-            const recipesRequired = Math.ceil(quantityNeeded / outputQuantityPerRecipe);
-            for (const ingredient of item.recipe.ingredients) {
-                const li = document.createElement('li');
-                // Adjusting the ingredient quantity based on the number of recipes required
-                const ingredientQuantity = ingredient.quantity * recipesRequired;
-                li.textContent = `${ingredientQuantity} ${ingredient.item}`;
-                li.appendChild(createList(ingredient.item, ingredientQuantity));  // Recursively create lists
-                ul.appendChild(li);
+        function createList(itemName, quantityNeeded) {
+            const item = data.item.find(i => i.name === itemName);
+            if (item && item.recipe && item.recipe.ingredients.length > 0) {
+                const ul = document.createElement('ul');
+                const outputQuantityPerRecipe = item.recipe.output.quantity;
+                // Calculating the number of recipes required
+                const recipesRequired = Math.ceil(quantityNeeded / outputQuantityPerRecipe);
+                for (const ingredient of item.recipe.ingredients) {
+                    const li = document.createElement('li');
+                    // Adjusting the ingredient quantity based on the number of recipes required
+                    const ingredientQuantity = ingredient.quantity * recipesRequired;
+                    li.textContent = `${ingredientQuantity} ${ingredient.item}`;
+                    li.appendChild(createList(ingredient.item, ingredientQuantity));  // Recursively create lists
+                    ul.appendChild(li);
+                }
+                return ul;
             }
-            return ul;
+            return document.createElement('ul');  // Return an empty list if there are no ingredients
         }
-        return document.createElement('ul');  // Return an empty list if there are no ingredients
+
+        const contentDiv = document.getElementById('content');
+        const topLevelList = document.createElement('ul');
+        const topLevelItem = document.createElement('li');
+        topLevelItem.textContent = `${quantity} ${targetItem}`;
+        topLevelItem.appendChild(createList(targetItem, quantity));  // Pass the quantity here
+        topLevelList.appendChild(topLevelItem);
+        contentDiv.appendChild(topLevelList);
+    } catch (error) {
+        console.error('There was an error in getProductionSteps:', error.message);
+        // Optionally, display an error message to the user
+        document.getElementById('content').innerText = 'An error occurred. Please try again.';
     }
-    
-    const contentDiv = document.getElementById('content');
-    const topLevelList = document.createElement('ul');
-    const topLevelItem = document.createElement('li');
-    topLevelItem.textContent = `${quantity} ${targetItem}`;
-    topLevelItem.appendChild(createList(targetItem, quantity));  // Pass the quantity here
-    topLevelList.appendChild(topLevelItem);
-    contentDiv.appendChild(topLevelList);
 }
